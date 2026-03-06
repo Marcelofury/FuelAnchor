a1761d6aa74564455ad59a3a69ea5458e3cdf404
Marcelofury <marcelofury13579@gmail.com>
2026-03-06 12:34:43 +0300

    updated merchant dashboard screen

--- frontend_flutter/lib/features/dashboard/presentation/screens/merchant_dashboard_screen.dart
+++ frontend_flutter/lib/features/dashboard/presentation/screens/merchant_dashboard_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import '../../../../core/constants/app_colors.dart';
-import '../../../../core/utils/responsive.dart';
 import '../../../auth/providers/providers.dart';
 
 class MerchantDashboardScreen extends ConsumerStatefulWidget {
