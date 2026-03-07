61e554ecc5001c640e567a584602ccbaabe2e5ea
Marcelofury <marcelofury13579@gmail.com>
2026-03-07 15:07:39 +0300

    Added database name conversion methods to UserRole enum for proper role storage (user_role.dart)

--- frontend_flutter/lib/core/enums/user_role.dart
+++ frontend_flutter/lib/core/enums/user_role.dart
@@ -16,4 +16,36 @@ enum UserRole {
         return 'Merchant';
     }
   }
+
+  /// Convert enum to database format (snake_case)
+  String get dbName {
+    switch (this) {
+      case UserRole.rider:
+        return 'rider';
+      case UserRole.fleetDriver:
+        return 'fleet_driver';
+      case UserRole.fleetManager:
+        return 'fleet_manager';
+      case UserRole.merchant:
+        return 'merchant';
+    }
+  }
+
+  /// Parse from database format (snake_case)
+  static UserRole fromDbName(String dbName) {
+    switch (dbName) {
+      case 'rider':
+        return UserRole.rider;
+      case 'fleet_driver':
+      case 'fleetDriver':
+        return UserRole.fleetDriver;
+      case 'fleet_manager':
+      case 'fleetManager':
+        return UserRole.fleetManager;
+      case 'merchant':
+        return UserRole.merchant;
+      default:
+        return UserRole.rider;
+    }
+  }
 }
